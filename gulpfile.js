c4259f91f868c3e7f01de00d196d224485f7a04d
PepoCivs <tcbpepo@gmail.com>
2017-04-25 23:43:24 +0200

    finish gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,26 @@
 var gulp = require('gulp');
 
-var jshint  = require('gulp-jshint');
-var sass    = require('gulp-sass');
-var concat  = require('gulp-concat');
-var uglify  = require('gulp-uglify');
-var rename  = require('gulp-rename');
 var replace = require('gulp-replace');
+var run     = require('gulp-run');
 
+gulp.task('set-analytics', function() {
+    run('git checkout feature/cordova-analytics').exec();
+});
 gulp.task('polymer-build', function() {
-    console.log('polyBuild');
+    run('polymer build').exec();
 });
 
 gulp.task('replace', function(){
   gulp.src([
-      '/build/default/index.html',
-      '/build/default/src/*.html',
-      '/build/default/src/**/*.html'
+      './build/default/service-worker.js',
+      './build/default/index.html',
+      './build/default/**/*.html'
     ])
     .pipe(replace('/src/', 'src/'))
     .pipe(replace('../images/items', '/android_asset/www/images/items'))
     .pipe(replace('https://api.', 'http://api.'))
     .pipe(replace('.custom-background{background-color:#33485c;}', '.custom-background{background-color:#33485c;margin-top:64px;}'))
-    .pipe(gulp.dest('/build/default'));
+    .pipe(gulp.dest('./build/default'));
 });
 
-gulp.task('build-cordova', ['polymer-build', 'replace']);
+gulp.task('build-cordova', ['set-analytics', 'polymer-build', 'replace']);
